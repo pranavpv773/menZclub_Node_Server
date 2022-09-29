cf848dfcb4dd0fc9d5bf2b7ce5ebdf06c1b56359
pranavpv773 <pranavpv773@gmail.com>
2022-09-29 21:49:52 +0530

    new branch created

--- model/productAddModel.js
+++ model/productAddModel.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose")
-
+const ratingSchema = require("./rating");
 const productInsertion = new mongoose.Schema(
     {
         product_name: { type: String , required : true},
+        product_price: { type: Number , required : true},
+        product_offer: { type: Number , required : true},
         product_category: { type: String , required : true},
         product_color: { type: String, required : true},
         product_brand: { type: String, required : true },
         product_size: { type: Number ,required : true},
         product_material: {type: String, required : true},
+        ratings: [ratingSchema],
         product_date: { type: Date, default: Date.now()}
     }
 )
