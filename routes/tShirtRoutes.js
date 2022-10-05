11101997e851f676b011136d47897c4913dc3059
pranavpv773 <pranavpv773@gmail.com>
2022-10-05 18:56:10 +0530

    tshirt router updated

--- routes/tShirtRoutes.js
+++ routes/tShirtRoutes.js
@@ -39,4 +39,45 @@ tShirtRouter.get("/api/menzclub/get-Tshirt", async (req, res) => {
         res.status(500).json({ status: "false", message: e.message });
     }
 });
+// /api/products/search/i
+tShirtRouter.get("/api/menzclub/t-shirt/", async (req, res) => {
+    try {
+        const tShirt = await Tshirt.find({ tShirt_category: req.query.tShirt_category });
+        res.status(200).json({tShirt, "status": true, "message": "Shirt added successfully"});
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+tShirtRouter.get("/api/menzclub/t-shirt/collection/", async (req, res) => {
+    try {
+        const tShirt = await Tshirt.find({ tShirt_collection: req.query.tShirt_collection });
+        res.status(200).json({tShirt, "status": true, "message": "Shirt added successfully"});
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+tShirtRouter.get("/api/menzclub/t-shirt/color/", async (req, res) => {
+    try {
+        const tShirt = await Tshirt.find({ tShirt_color: req.query.tShirt_color });
+        res.status(200).json({ tShirt, "status": true, "message": "Shirt added successfully" });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+tShirtRouter.get("/api/menzclub/t-shirt/material/", async (req, res) => {
+    try {
+        const tShirt = await Tshirt.find({ tShirt_material: req.query.tShirt_material });
+        res.status(200).json({ tShirt, "status": true, "message": "Shirt added successfully" });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+tShirtRouter.get("/api/menzclub/t-shirt/fit/", async (req, res) => {
+    try {
+        const tShirt = await Tshirt.find({ tShirt_fit: req.query.tShirt_fit });
+        res.status(200).json({ tShirt, "status": true, "message": "Shirt added successfully" });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
 module.exports = tShirtRouter;
