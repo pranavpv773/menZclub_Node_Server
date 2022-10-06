3f4ec2b9ee0508056b1bf9c88a2775768257fc24
pranavpv773 <pranavpv773@gmail.com>
2022-10-06 10:28:35 +0530

    api for filters in jeans developed

--- routes/jeansRouter.js
+++ routes/jeansRouter.js
@@ -40,4 +40,44 @@ jeansRouter.get("/api/menzclub/get-jeans", async (req, res) => {
         res.status(500).json({ "status":false,message: e.message });
     }
 });
+jeansRouter.get("/api/menzclub/jeans/", async (req, res) => {
+    try {
+        const jeans = await Jeans.find({ jeans_category: req.query.jeans_category });
+        res.status(200).json({jeans, "status": true, "message": "jeans_category added successfully"});
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+jeansRouter.get("/api/menzclub/jeans/collection/", async (req, res) => {
+    try {
+        const jeans = await Jeans.find({ jeans_collection: req.query.jeans_collection });
+        res.status(200).json({jeans, "status": true, "message": "jeans_collection added successfully"});
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+jeansRouter.get("/api/menzclub/jeans/color/", async (req, res) => {
+    try {
+        const jeans = await Jeans.find({ jeans_color: req.query.jeans_color });
+        res.status(200).json({ jeans, "status": true, "message": "jeans_color added successfully" });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+jeansRouter.get("/api/menzclub/jeans/material/", async (req, res) => {
+    try {
+        const jeans = await Jeans.find({ jeans_material: req.query.jeans_material });
+        res.status(200).json({ jeans, "status": true, "message": "jeans_material added successfully" });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
+jeansRouter.get("/api/menzclub/jeans/fit/", async (req, res) => {
+    try {
+        const jeans = await Jeans.find({ jeans_fit: req.query.jeans_fit });
+        res.status(200).json({ jeans, "status": true, "message": "jeans_fit added successfully" });
+    } catch (e) {
+        res.status(500).json({ error: e.message });
+    }
+});
 module.exports = jeansRouter;
