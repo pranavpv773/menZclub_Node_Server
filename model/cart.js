911cd58231794710c90bae679dcc7cafc68a826c
pranavpv773 <pranavpv773@gmail.com>
2022-10-21 17:23:08 +0530

    cart updated to unique

--- model/cart.js
+++ model/cart.js
@@ -1,20 +1,16 @@
 const mongoose = require("mongoose")
 //const {productSchema} =require("./productAddModel")
 const cartSchema = new mongoose.Schema(
-    {
-        user_mail: { type: String , required : true },
-        user_cart: [
-            // {
-            //   product: productSchema,
-            //   // quantity: {
-            //   //   type: Number,
-            //   //   required: true,
-            //   // },
-            // },
-          ],
-    }
+  {
+    user_mail: { type: String, required: true },
+    user_cart: [
+      {
+        type: String,
+        required: true,
+      },],
+  }
 )
 
 const Cart = mongoose.model("cartList", cartSchema)
 
-module.exports = {Cart,cartSchema};
+module.exports = { Cart, cartSchema };
